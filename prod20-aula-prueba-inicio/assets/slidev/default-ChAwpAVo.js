ff733d01471c8459fe3851fd857ffa313df572d8
horacio <corteshoracio@gmail.com>
2026-07-29 04:30:18 -0300

    20260729043018

---
+++ prod20-aula-prueba-inicio/assets/slidev/default-ChAwpAVo.js
@@ -0,0 +1,1 @@
+import{I as e,v as t,z as n}from"../modules/shiki-BdTzSZJx.js";import{ot as r}from"../index-6hLvmvu7.js";var i={},a={class:`slidev-layout default`};function o(r,i){return e(),t(`div`,a,[n(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
